8f48294e797c1644ca0266107c87e044f2e9f2f5
Scott Anderson <simple.scott.94@googlemail.com>
2025-03-04 18:16:33 +0000

    main - have Plugin load data from the App Server `stock-status` endpoint

---
+++ Resources/app/storefront/src/main.js
@@ -0,0 +1,7 @@
+// <plugin root>/src/Resources/app/storefront/src/main.js
+// Import all necessary Storefront plugins
+import StockCheck from './stock-check/stock-check.plugin.js';
+
+// Register your plugin via the existing PluginManager
+const PluginManager = window.PluginManager;
+PluginManager.register('StockCheck', StockCheck, '.product-detail-ordernumber');

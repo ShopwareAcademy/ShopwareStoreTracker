e7fa92dcd0728a9b7577c4f2cb1be5676edfd0cb
Beytullah Cevik <b.cevik@shopware.com>
2025-11-07 18:14:47 +0100

    Fix: update code

--- Resources/app/storefront/src/main.js
+++
@@ -1,7 +0,0 @@
-// <plugin root>/src/Resources/app/storefront/src/main.js
-// Import all necessary Storefront plugins
-import StockCheck from './stock-check/stock-check.plugin.js';
-
-// Register your plugin via the existing PluginManager
-const PluginManager = window.PluginManager;
-PluginManager.register('StockCheck', StockCheck, '[data-stock-check]');
